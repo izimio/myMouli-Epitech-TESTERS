de4be0f6c9bbd0833b9931c56a42d5915a65493f
Izimio <joshua.brionne@gmail.com>
2023-03-07 17:12:56 +0100

    [UPGRADE] init.py

--- init.py
+++ init.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 
@@ -15,22 +17,33 @@ def get_args():
     return args
 
 def remove_directories(directories, args):
-    dir_found = False
-
-    for directory in directories:
-        if directory.lower() in args:
-            dir_found = True
-    if not dir_found:
-        print("No directory found with args: ", args)
-        sys.exit(1)
     for directory in directories:
         if directory.lower() not in args:
             os.system("rm -rf " + directory)
+    os.system("rm -f README.md init.py")
 
 args = get_args()
 directories = get_directories()
+dir_found = False
 
-print("Directories: ", directories)
-print("Args: ", args)
+def ask_is_ok():
+    while True:
+        answer = input("Are you sure you want to delete these directories? (y/n): ")
+        if answer in ("y", "n"):
+            break
+        print("Invalid input. Please enter y or n.")
+    if answer == "y":
+        return True
+    else:
+        exit(1)
+    
+for directory in directories:
+    if directory.lower() in args:
+        dir_found = True
+if not dir_found:
+    print("No directory found with args: ", args)
+    exit(1)
 
+print("Folders to keep: ", args)
+ask_is_ok()
 remove_directories(directories, args)
